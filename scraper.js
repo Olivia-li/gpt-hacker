f0628de6a0b212e7dec67d989fcf1a4b09886e37
Olivia Li <oliviali.ouyang@gmail.com>
2024-02-15 03:33:02 -0500

    gather all hacking tutorails

--- scraper.js
+++ scraper.js
@@ -15,5 +15,26 @@ function grabLinks() {
 function grabTutorialText() {
   const firstPost = document.querySelector(".post_content")
   const postText = firstPost.innerText || "N/A"
-  return postText 
+  console.log(postText)
 }
+
+// Arc browser boost code to copy data to clipboard
+document.addEventListener('keydown', async (event) => {
+  // Check for CMD + B on MacOS or CTRL + B on Windows/Linux
+  if ((event.metaKey || event.ctrlKey) && event.key === 'b') {
+    event.preventDefault(); // Prevent the default action to avoid triggering browser behavior
+
+    const textToCopy = {
+      title: document.title,
+      text: document.querySelector(".post_content") ? document.querySelector(".post_content").innerText : "N/A"
+    };
+
+    console.log(textToCopy);
+    try {
+      await navigator.clipboard.writeText(JSON.stringify(textToCopy));
+      console.log("Text copied to clipboard");
+    } catch (err) {
+      console.error("Failed to copy text: ", err);
+    }
+  }
+});
